a935cdb9712967da9cfdca090699a1933d15b27a
Jacob L Moore <moore.jlayne@gmail.com>
2021-06-15 19:02:00 -0500

    rename default files; remove unnecessary files; update readme

---
+++ src/TabletWeaver.js
@@ -0,0 +1,17 @@
+import './tablet-weaver.css';
+
+function TabletWeaver() {
+    return (
+        <div className="tablet-weaver">
+            <header className="tablet-weaver-header">
+                <h1>Tablet Weaver</h1>
+                <p>
+                    Coming soon!
+                </p>
+            </header>
+
+        </div>
+    );
+}
+
+export default TabletWeaver;
--- src/TabletWeaver.test.js
+++ src/TabletWeaver.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import TabletWeaver from './TabletWeaver';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<TabletWeaver />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
